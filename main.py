99971caec6c9b1f7e9e4a18be1cfa3b545dfc1be
GemechisUrgessa <gemechisurgessa@gmail.com>
2022-06-18 21:08:13 +0300

    some updates made

--- main.py
+++ main.py
@@ -64,20 +64,29 @@ while True:
             sys.exit()
 
     keys = pygame.key.get_pressed()  # checking pressed keys
+
     if keys[pygame.K_UP]:
         # if car_z_pos >= -4 or (car_z_pos < -4 and car_x_pos < -20):
         if car_x_rotation < 50.0 and car_x_position > -27:
             car_x_position -= 1
         elif car_x_rotation >= 45.0 and car_x_rotation < 145 and car_z_position > -22:
-            car_z_position -= 1
+            if car_x_position > -25 and car_x_position < 5:
+                pass
+            else:
+                car_z_position -= 1
             # car_y_pos += 0.3
         elif car_x_rotation > 145 and car_x_rotation < 245 and car_x_position < 12:
             car_x_position += 1
         elif car_x_rotation > 245 and car_x_rotation < 350 and car_z_position < -3:
-            car_z_position += 1
+            if car_x_position > -25 and car_x_position < 5:
+                pass
+            else:
+                car_z_position += 1
             # car_y_pos -= 0.3
         elif car_x_rotation >= 350:
             car_x_rotation = 0
+        # elif car_x_rotation > -45:
+        #     car_z_position += 1
     if keys[pygame.K_DOWN]:
         # if car_z_pos >= -4 or (car_z_pos < -4 and car_x_pos < -20):
         if car_x_rotation <= 50.0 and car_x_position >= -27 and car_x_position < 7:
@@ -92,8 +101,10 @@ while True:
             # car_y_pos -= 0.3
         elif car_x_rotation >= 350:
             car_x_rotation = 0
+
     if keys[pygame.K_RIGHT]:
         car_x_rotation += 5
+
     if keys[pygame.K_LEFT]:
         car_x_rotation -= 5
 
